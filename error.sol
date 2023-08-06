a8cacb27c99b8ce1d0f8907613872f543ea7b139
sudhanshu6634 <100783146+sudhanshu6634@users.noreply.github.com>
2023-08-06 09:44:10 +0530

    error.sol

--- error.sol
+++ error.sol
@@ -1,41 +1,25 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity 0.8.18;
 
-contract Error {
-    function testRequire(uint _i) public pure {
-        // Require should be used to validate conditions such as:
-        // - inputs
-        // - conditions before execution
-        // - return values from calls to other functions
-        require(_i > 10, "Input must be greater than 10");
+contract ZeroDivisionErrorHandling{ 
+    uint public result;
+    function testRequire(uint numerator, uint denominator) public {
+         //using require function to handle the error.
+        require(denominator != 0, "Denominator must be non-zero.");
+        result = numerator / denominator;
     }
 
-    function testRevert(uint _i) public pure {
-        // Revert is useful when the condition to check is complex.
-        // This code does the exact same thing as the example above
-        if (_i <= 10) {
-            revert("Input must be greater than 10");
+    function testAssert(uint numerator, uint denominator) public {
+         //using assert function to handle the error.
+        assert(denominator != 0);
+        result = numerator / denominator;
+    }
+
+    function testRevert(uint numerator, uint denominator) public {
+        if (denominator == 0) {
+             //using revert function to handle the error.
+            revert("Denominator must be non-zero.");
         }
+        result = numerator / denominator;
     }
 
-    uint public num;
-
-    function testAssert() public view {
-        // Assert should only be used to test for internal errors,
-        // and to check invariants.
-
-        // Here we assert that num is always equal to 0
-        // since it is impossible to update the value of num
-        assert(num == 0);
-    }
-
-    // custom error
-    error InsufficientBalance(uint balance, uint withdrawAmount);
-
-    function testCustomError(uint _withdrawAmount) public view {
-        uint bal = address(this).balance;
-        if (bal < _withdrawAmount) {
-            revert InsufficientBalance({balance: bal, withdrawAmount: _withdrawAmount});
-        }
-    }
 }
